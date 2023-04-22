1b4c5124a3adf3ff04e218ff7cb97e77ee7237e3
Aleksandr Erofeev <erofeevs943@gmail.com>
2023-04-21 22:17:06 -0700

    (ADD) Models for Products and Category

--- products/models.py
+++ products/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
+
 # Create your models here.
+
+class ProductCategory(models.Model):
+    name = models.CharField(max_length=256)
+    description = models.TextField()
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=256)
+    description = models.TextField()
+    quantity = models.PositiveIntegerField()
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    category = models.ForeignKey(to=ProductCategory, on_delete=models.PROTECT)
+    img = models.ImageField(upload_to=r'product_img')
