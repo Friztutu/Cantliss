f5b90e5550e0646882641884f092af073863d3f1
Aleksandr Erofeev <erofeevs943@gmail.com>
2023-04-26 03:53:42 -0700

    (ADD) Create a basket model

--- basket/models.py
+++ basket/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from products.models import Product
+from users.models import CustomUser
+
 
 # Create your models here.
+
+class BasketProduct(models.Model):
+    product = models.ForeignKey(to=Product, on_delete=models.CASCADE)
+    user = models.ForeignKey(to=CustomUser, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f'{self.user.name} || {self.product.name}'
+
+    def get_total_sum(self):
+        return self.product.price * self.quantity
