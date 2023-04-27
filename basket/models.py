7054fea91036731c32f6fbeaf6211d3e77797a12
Aleksandr Erofeev <erofeevs943@gmail.com>
2023-04-27 06:17:58 -0700

    (ADD) BasketQuerySet, make total sum and total quantity in template

--- basket/models.py
+++ basket/models.py
@@ -5,23 +5,25 @@ from users.models import CustomUser
 
 # Create your models here.
 
+
+class BasketQuerySet(models.QuerySet):
+    def get_total_sum(self):
+        return sum(basket.get_product_sum() for basket in self)
+
+    def get_total_quantity(self):
+        return sum(basket.quantity for basket in self)
+
+
 class Basket(models.Model):
     product = models.ForeignKey(to=Product, on_delete=models.CASCADE)
     user = models.ForeignKey(to=CustomUser, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
 
+    objects = BasketQuerySet.as_manager()
+
     def __str__(self):
         return f'{self.user.name} || {self.product.name}'
 
     def get_product_sum(self):
         return self.product.price * self.quantity
-
-    def get_total_quantity(self):
-        baskets = self.objects.filter(user=self.user)
-        return sum(basket.quantity for basket in baskets)
-
-    def get_total_sum(self):
-        baskets = self.objects.filter(user=self.user)
-        return sum(basket.get_product_sum() for basket in baskets)
-
