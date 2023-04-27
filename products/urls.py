d8466a571f878f41a4b82639551fc6ec4b7d5782
Aleksandr Erofeev <erofeevs943@gmail.com>
2023-04-27 07:34:49 -0700

    (ADD) Add products catalog pagination

--- products/urls.py
+++ products/urls.py
@@ -7,5 +7,6 @@ app_name = 'catalog'
 urlpatterns = [
     path('', index, name='index'),
     path('catalog/', catalog, name='catalog'),
-    path('catalog/category/int:<category_id>', catalog, name='category')
+    path('catalog/category/int:<category_id>', catalog, name='category'),
+    path('catalog/page/int:<page>', catalog, name='paginator')
 ]
--- products/views.py
+++ products/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from products.models import Product, ProductCategory
+from django.core.paginator import Paginator
 
 
 # Create your views here.
@@ -11,11 +12,14 @@ def index(request):
     return render(request, 'products/index.html', context=context)
 
 
-def catalog(request, category_id=None):
+def catalog(request, category_id=None, page=1):
     products = Product.objects.filter(category_id=category_id) if category_id else Product.objects.all()
+    paginator = Paginator(object_list=products, per_page=3)
+    products_paginator = paginator.page(page)
+
     context = {
         'title': 'Каталог',
-        'products': products,
+        'products': products_paginator,
         'categories': ProductCategory.objects.all(),
     }
     return render(request, 'products/catalog.html', context=context)
