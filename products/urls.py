2d963611010b5209ee43e0bddc0eb028094bfd0a
Aleksandr Erofeev <erofeevs943@gmail.com>
2023-04-29 01:57:08 -0700

    (Refactor) Index and catalog view Func based view -> Class based view

--- products/urls.py
+++ products/urls.py
@@ -1,12 +1,11 @@
 from django.urls import path
-from products.views import IndexView, catalog
+from products.views import IndexView, CatalogView
 
 
 app_name = 'catalog'
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
-    path('catalog/', catalog, name='catalog'),
-    path('catalog/category/int:<category_id>', catalog, name='category'),
-    path('catalog/page/int:<page>', catalog, name='paginator')
+    path('catalog/', CatalogView.as_view(), name='catalog'),
+    path('catalog/category/int:<category_id>', CatalogView.as_view(), name='category'),
 ]
--- products/views.py
+++ products/views.py
@@ -2,12 +2,36 @@ from django.shortcuts import render
 from products.models import Product, ProductCategory
 from django.core.paginator import Paginator
 from django.views.generic.base import TemplateView
+from django.views.generic.list import ListView
 
 
 # Create your views here.
 
 class IndexView(TemplateView):
     template_name = 'products/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(IndexView, self).get_context_data()
+        context['title'] = 'Главная'
+        return context
+
+
+class CatalogView(ListView):
+    model = Product
+    template_name = 'products/catalog.html'
+    paginate_by = 3
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(CatalogView, self).get_context_data()
+        context['title'] = 'Каталог'
+        context['categories'] = ProductCategory.objects.all()
+        return context
+
+    def get_queryset(self):
+        queryset = super(CatalogView, self).get_queryset()
+        category_id = self.kwargs.get('category_id')
+        queryset = queryset.filter(category_id=category_id) if category_id else queryset
+        return queryset
 
 
 def catalog(request, category_id=None, page=1):
