861c6d1349656b3bd7b62c3f9b68d140f000ee72
Aleksandr Erofeev <erofeevs943@gmail.com>
2023-04-29 01:59:45 -0700

    (Refactor) Del func catalog view

--- products/views.py
+++ products/views.py
@@ -33,15 +33,3 @@ class CatalogView(ListView):
         queryset = queryset.filter(category_id=category_id) if category_id else queryset
         return queryset
 
-
-def catalog(request, category_id=None, page=1):
-    products = Product.objects.filter(category_id=category_id) if category_id else Product.objects.all()
-    paginator = Paginator(object_list=products, per_page=3)
-    products_paginator = paginator.page(page)
-
-    context = {
-        'title': 'Каталог',
-        'products': products_paginator,
-        'categories': ProductCategory.objects.all(),
-    }
-    return render(request, 'products/catalog.html', context=context)
