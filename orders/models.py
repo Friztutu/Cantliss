ce388f0470d501356768fb14c6167f39275a5dd1
Alex <erofeevs943@gmail.com>
2023-05-09 16:37:48 +0300

    (ADD) Models for orders app

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from users.models import CustomUser
+from products.models import Product
+
 
 # Create your models here.
+
+class Order(models.Model):
+    user = models.ForeignKey(to=CustomUser, on_delete=models.PROTECT)
+    products = models.ForeignKey(to=Product, on_delete=models.PROTECT)
+    create_at = models.DateTimeField(auto_now_add=True)
+    address = models.TextField()
+    is_paid = models.BooleanField(default=False)
